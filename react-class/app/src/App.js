d368c0cfc873223fdec889a68eae3ce3ddf88192
GOPI-I <igopi170@gmail.com>
2024-08-21 16:30:29 +0530

    21-08-24 commit

---
+++ react-class/app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './one.jpg'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>This is react app</h1>
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
