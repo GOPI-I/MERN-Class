dbaa0f3fa029b5ede97956d681162c9934bc6160
GOPI-I <igopi170@gmail.com>
2024-08-22 19:11:37 +0530

    22/08/24 commit

--- react-class/app/src/App.js
+++ react-class/app/src/App.js
@@ -1,14 +1,15 @@
-import logo from './one.jpg'
+
 import './App.css';
+import Gome from './Gome';
+import Login from './Login';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>This is react app</h1>
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-    </div>
+  
+  return (<>
+    {/* <Gome/> */}
+    <Login/>
+    </>
   );
 }
 
