709887a71b3008b841b1a990d9d2d209555c72f7
GOPI-I <igopi170@gmail.com>
2024-08-27 16:19:20 +0530

    commit

--- react-class/app/src/App.js
+++ react-class/app/src/App.js
@@ -2,13 +2,17 @@
 import './App.css';
 import Gome from './Gome';
 import Login from './Login';
+import JokeComponent from './JokeComponent';
+import Register1 from './Register1';
 
 
 function App() {
   
   return (<>
+  <Register1/>
+  {/* <JokeComponent/> */}
     {/* <Gome/> */}
-    <Login/>
+    {/* <Login/> */}
     </>
   );
 }
